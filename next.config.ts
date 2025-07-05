50cce330568d72019e8ad707451398c930dc52f8
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-07-06 00:17:28 +0700

    setting config

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/storage/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
+        protocol: "https",
+        hostname: "appsdealer.rizafidev.site",
         pathname: "/storage/**",
       },
     ],
